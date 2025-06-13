befead65da014eeba47964a11e30d8dc08460bd0
Alex Wandugu (thiongo) <lexwan23@gmail.com>
2025-06-13 17:18:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/components/customlogo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const CustomLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 226 207"
+    fill="currentColor"
+    {...props}
+  >
+    <path d="src/components/image-svg.svg" />
+  </svg>
+);
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
 import { Zap } from 'lucide-react';
+import Image from 'next/image';
+import CustomLogo from 'src/components/image.svg';
+// import {ReactComponent as CustomLogo} from 'src/components/image-svg.svg';
 
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={`flex items-center gap-2 text-2xl font-headline font-bold text-primary hover:text-primary/80 transition-colors ${className}`}>
       <Zap className="h-7 w-7" />
-      CircuitFlow
+      {/* <img src = 'src/components/image-removebg-preview (40)-modified.png'/> */}
+      {/* <CustomLogo /> */}
+      <Image src="src/components/image.svg" alt="Dropbox Draughtsman Logo" width={40} height={40} />
+      Dropbox Draughtsman
     </Link>
   );
 }

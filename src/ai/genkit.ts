b1aa48e5b2ca73b4b3906ff819ebb21ad7f54d53
Alex Wandugu (thiongo) <lexwan23@gmail.com>
2025-06-14 06:36:34 +0000

    Set up Gemini Env Variable

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,4 +4,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [googleAI()],
   model: 'googleai/gemini-2.0-flash',
+  logLevel: 'debug',
+  flowStateStore: 'firebase',
+  traceStore: 'firebase',
 });

2453253b5fb26a543faf8a926de4c28b44551e1a
Alex Wandugu (thiongo) <lexwan23@gmail.com>
2025-06-29 12:08:30 +0000

    Site - Mapped✔

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://dropboxdraughtsman.vercel.app/',
+    generateRobotsTxt: true, // also generates robots.txt
+    sitemapSize: 5000,
+  };
+  
